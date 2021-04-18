4f2cebd70d095a203a5c2bc58bc1a66a08ae6ee9
Clayton Ramsey <clayton.j.ramsey@gmail.com>
2021-04-17 21:00:31 -0400

    Text input from files.

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 # A Shiny app for demonstrating the letter following frequency algorithm I wrote.
 
 library(shiny)
+library(readtext)
 
 ui <- fluidPage(
   titlePanel("Letter Following Frequency Demo"),
@@ -11,36 +12,47 @@ ui <- fluidPage(
                    label = "Select the text to analyze:",
                    choiceNames = c("Chance For Peace, Dwight D. Eisenhower, 1953",
                                    "The Gettysburg Address, Abraham Lincoln, 1863",
-                                   "The Declaration of Independence, 1776"
+                                   "The Declaration of Independence, 1776",
+                                   "Custom"
                                    ),
-                   choiceValues = c("chance_for_peace", "gettysburg_address", "declaration_of_independence")
-                   )
+                   choiceValues = c("chance_for_peace", 
+                                    "gettysburg_address", 
+                                    "declaration_of_independence",
+                                    "custom")
+                   ),
+      textInput(inputId = "custom",
+                label = "Enter the custom text here:",
+                value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed turpis diam, pretium quis felis a, congue varius justo. Etiam sollicitudin ex et pretium varius.")
     ),
     mainPanel(
       h3("Analysis Results:"),
       p("Text Chosen:"),
-      textOutput("text_choice")
+      textOutput("text_choice"),
+      textOutput("custom_text")
     )
   )
 )
 
 
 server <- function(input, output){
-  ####### Code to load the blocks of text based on what is selected in the UI. ######
-  
-  # Code to load the text from files will go here.
-  
-  # This list currently contains dummy text but will later have what is loaded from above
-  text_samples <- list(
-    chance_for_peace = "text of the cross of iron speech",
-    gettysburg_address = "text of the gettysburg address",
-    declaration_of_independence = "text from the declaration"
-  )
 
-    ###### Text output widget. ######
-  output$text_choice <- renderText({
-    text_samples[[input$text_select]]
+  rvar_storage <- reactiveValues(
+    custom_text = character()
+  )
+  
+  text_samples <- list(
+    chance_for_peace = readtext("cfp.txt")$text,
+    gettysburg_address = readtext("ga.txt")$text,
+    declaration_of_independence = readtext("doi.txt")$text
+  )
+  rvar_storage$custom_text <- renderText({input$custom})
+
+  ###### Text output widget. ######
+    output$text_choice <- renderText({
+      text_samples[[input$text_select]]
   }) 
+    #output$custom_text <- renderText(rvar_storage$custom_text)
+  output$custom_text <- renderText(input$custom)
   
 }
 
